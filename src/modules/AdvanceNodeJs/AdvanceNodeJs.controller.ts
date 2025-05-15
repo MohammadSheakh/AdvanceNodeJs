9e91087bb8404108cd780687419c0ea7e13c1931
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-05-15 14:24:35 +0600

    working on worker js

--- src/modules/AdvanceNodeJs/AdvanceNodeJs.controller.ts
+++ src/modules/AdvanceNodeJs/AdvanceNodeJs.controller.ts
@@ -8,6 +8,10 @@ import { AdvanceNodeJsService } from './AdvanceNodeJs.service';
 import catchAsync from '../../shared/catchAsync';
 import sendResponse from '../../shared/sendResponse';
 
+import path from 'path';
+
+import { Worker } from 'worker_threads';
+
 // let conversationParticipantsService = new ConversationParticipentsService();
 // let messageService = new MessagerService();
 
@@ -23,15 +27,44 @@ export class AdvanceNodeJsController extends GenericController<
 
   blocking = catchAsync(async (req: Request, res: Response) => {
     console.log('blocking');
-    let counter = 0;
-    for (let i = 0; i < 20000000000; i++) {
-      counter++;
-    }
-    sendResponse(res, {
-      code: StatusCodes.BAD_REQUEST,
-      message: `this is blocking and result is ${counter}`,
-      success: false,
+    /*
+      let counter = 0;
+      for (let i = 0; i < 20000000000; i++) {
+        counter++;
+      }
+    */
+    /**
+     * we will do this heavy computation in worker thread
+     * and we will use the parent port to send the result back
+     */
+
+    //const worker = new Worker('./worker.ts')
+
+    const worker = new Worker(path.resolve(__dirname, 'worker.ts'))
+
+    // we can listen to the message event on the worker // listen to some specific event 
+
+    worker.on('message', (data:any) => {
+      sendResponse(res, {
+        code: StatusCodes.OK,
+        message: `this is blocking and result is ${data.counter}`,
+        success: false,
+      });
     });
+
+    worker.on('error', (error:any) => {
+      sendResponse(res, {
+        code: StatusCodes.NOT_FOUND,
+        message: `this is blocking and an error occured ${error}`,
+        success: false,
+      });
+    });
+
+    // sendResponse(res, {
+    //   code: StatusCodes.BAD_REQUEST,
+    //   message: `this is blocking and result is ${counter}`,
+    //   success: false,
+    // });
   });
   nonBlocking = catchAsync(async (req: Request, res: Response) => {
     console.log('this is non-blocking');
---
+++ src/modules/AdvanceNodeJs/worker.ts
@@ -0,0 +1,20 @@
+const {parentPort} = require('worker_threads');
+
+let counter = 0;
+for (let i = 0; i < 20000000000; i++) {
+  counter++;
+}
+
+parentPort.postMessage({counter}); 
+/**
+ * postMessage() is the way to communicate with the main thread.
+ * from worker 
+ */
+
+/**
+ * 
+ * lets find out how many core we have ... 
+ * 
+ * > sysctl -n hw.ncpu
+ * > echo %NUMBER_OF_PROCESSORS% in terminal for windows .. 
+ */
